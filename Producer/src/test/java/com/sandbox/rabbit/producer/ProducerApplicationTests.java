d5728935854efea1e4480c9c9379d0914ec9dc58
Novke <novke.brate@gmail.com>
2025-10-14 18:35:13 +0200

    init consumer & producer

---
+++ Producer/src/test/java/com/sandbox/rabbit/producer/ProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sandbox.rabbit.producer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
