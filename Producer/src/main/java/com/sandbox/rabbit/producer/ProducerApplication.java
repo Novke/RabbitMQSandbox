3be85dcec6834f67a51ca97a770519ebb4c13680
Novke <novke.brate@gmail.com>
2025-10-14 19:05:13 +0200

    Hello Producer

--- Producer/src/main/java/com/sandbox/rabbit/producer/ProducerApplication.java
+++ Producer/src/main/java/com/sandbox/rabbit/producer/ProducerApplication.java
@@ -1,13 +1,27 @@
 package com.sandbox.rabbit.producer;
 
+import com.sandbox.rabbit.producer.producer.HelloRabbitProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 @SpringBootApplication
-public class ProducerApplication {
+public class ProducerApplication implements CommandLineRunner {
+
+
+    @Autowired
+    private HelloRabbitProducer helloRabbitProducer;
 
     public static void main(String[] args) {
         SpringApplication.run(ProducerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+           helloRabbitProducer.sendHello("Name " + ThreadLocalRandom.current().nextInt());
     }
 
 }
